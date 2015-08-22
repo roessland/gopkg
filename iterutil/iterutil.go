f1ccd41db902006c31e6b87b71409227d76b323c
Andreas Røssland <andreas.roessland@gmail.com>
2015-08-22 07:41:51 -0400

    add cartesian power

--- iterutil/iterutil.go
+++ iterutil/iterutil.go
@@ -1,7 +1,9 @@
 package iterutil
 
 import (
-    "gitub.com/roessland/gopkg/sliceutil"
+    "math"
+    "github.com/roessland/gopkg/mathutil"
+    "github.com/roessland/gopkg/sliceutil"
 )
 
 // Return all the elements of the cartesian p-th power of Z_n
@@ -10,16 +12,27 @@ import (
 // {  (0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)  }.
 //
 // The result has length n**power.
-func CartesianPower(n int, power int) <-chan []int {
-    // Number of elements in the generated set
-    cardinality := int(math.Pow(float64(n), float64(power)))
+func CartesianPower(n int64, power int64) <-chan []int64 {
+    if n <= 0 {
+        panic("n must be positive")
+    }
+    if power <= 0 {
+        panic("power must be positive")
+    }
 
-	out := make(chan []int)
+    // Number of elements in the generated set
+    cardinality := int64(math.Pow(float64(n), float64(power)))
+
+	out := make(chan []int64)
 	go func() {
-		for i := 0; i < cardinality; i = i + 1 {
-			out <- sliceutil.ZeroPadLeftInt64(mathutil.ToDigits(i, n), power, 0)
-		}
-		close(out)
+        if n == 1 {
+            out <- make([]int64, power)
+        } else {
+            for i := int64(0); i < cardinality; i = i + 1 {
+                out <- sliceutil.ZeroPadLeftInt64(mathutil.ToDigits(i, n), power)
+            }
+        }
+        close(out)
 	}()
 	return out
 }
--- iterutil/iterutil_test.go
+++ iterutil/iterutil_test.go
@@ -6,8 +6,32 @@ import (
 )
 
 func TestCartesianPower(t *testing.T) {
-    // (Z_1)**0 is an empty set
-    tuples := CartesianPower(1, 0)
-    emptySet <- tuples
-    assert.Equal(t, emptySet, nil, "they should be equal")
+    var tuples <-chan []int64
+
+    // (Z_1)**1 = { (0) }
+    tuples = CartesianPower(1, 1)
+    assert.Equal(t, []int64{0}, <-tuples, "they should be equal")
+    assert.Nil(t, <-tuples, "channel should be closed")
+
+    // (Z_1)**2 = { (0,0) }
+    tuples = CartesianPower(1, 2)
+    assert.Equal(t, []int64{0, 0}, <-tuples, "they should be equal")
+    assert.Nil(t, <-tuples, "channel should be closed")
+
+    // (Z_2)**2 = { (0,0), (0,1), (1,0), (1,1) }
+    tuples = CartesianPower(2, 2)
+    assert.Equal(t, []int64{0, 0}, <-tuples, "they should be equal")
+    assert.Equal(t, []int64{0, 1}, <-tuples, "they should be equal")
+    assert.Equal(t, []int64{1, 0}, <-tuples, "they should be equal")
+    assert.Equal(t, []int64{1, 1}, <-tuples, "they should be equal")
+    assert.Nil(t, <-tuples, "channel should be closed")
+
+    // (Z_5)**1 = { (0), (1), (2), (3), (4) }
+    tuples = CartesianPower(5, 1)
+    assert.Equal(t, []int64{0}, <-tuples, "they should be equal")
+    assert.Equal(t, []int64{1}, <-tuples, "they should be equal")
+    assert.Equal(t, []int64{2}, <-tuples, "they should be equal")
+    assert.Equal(t, []int64{3}, <-tuples, "they should be equal")
+    assert.Equal(t, []int64{4}, <-tuples, "they should be equal")
+    assert.Nil(t, <-tuples, "channel should be closed")
 }
