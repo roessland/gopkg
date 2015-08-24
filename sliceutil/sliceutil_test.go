dae411ec8c8cb5eaefc211c46de0edbac7e1a1b6
Andreas Røssland <andreas.roessland@gmail.com>
2015-08-24 17:17:15 +0200

    indentation fix

--- sliceutil/sliceutil_test.go
+++ sliceutil/sliceutil_test.go
@@ -3,25 +3,25 @@ package sliceutil
 import "testing"
 
 func TestEqualInt_different_length(t *testing.T) {
-    a := []int{1, 2, 3}
-    b := []int{1, 2}
-    if EqualInt(a, b) {
-        t.Errorf("IntsEqual(%v, %v) == true, want false")
-    }
+	a := []int{1, 2, 3}
+	b := []int{1, 2}
+	if EqualInt(a, b) {
+		t.Errorf("IntsEqual(%v, %v) == true, want false")
+	}
 }
 
 func TestEqualInt_same_length_but_different(t *testing.T) {
-    a := []int{1, 2, 3}
-    b := []int{1, 2, 4}
-    if EqualInt(a, b) {
-        t.Errorf("IntsEqual(%v, %v) == true, want false")
-    }
+	a := []int{1, 2, 3}
+	b := []int{1, 2, 4}
+	if EqualInt(a, b) {
+		t.Errorf("IntsEqual(%v, %v) == true, want false")
+	}
 }
 
 func TestEqualInt_same(t *testing.T) {
-    a := []int{1, 2, 3, 1}
-    b := []int{1, 2, 3, 1}
-    if !EqualInt(a, b) {
-        t.Errorf("IntsEqual(%v, %v) == false, want true")
-    }
+	a := []int{1, 2, 3, 1}
+	b := []int{1, 2, 3, 1}
+	if !EqualInt(a, b) {
+		t.Errorf("IntsEqual(%v, %v) == false, want true")
+	}
 }
