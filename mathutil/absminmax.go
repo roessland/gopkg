f9528285cb6f056fb29bd1e17232e26adb789a2c
Andreas Røssland <andreas.roessland@gmail.com>
2021-12-05 13:28:38 +0100

    add sign and max and abs for ints

--- mathutil/absminmax.go
+++ mathutil/absminmax.go
@@ -8,10 +8,46 @@ func AbsInt64(n int64) int64 {
 	}
 }
 
+func AbsInt(n int) int {
+	if n < 0 {
+		return -n
+	} else {
+		return n
+	}
+}
+
 func MaxInt64(a, b int64) int64 {
 	if a > b {
 		return a
 	} else {
 		return b
 	}
+}
+
+func MaxInt(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func SignInt64(n int64) int64 {
+	if n == 0 {
+		return 0
+	}
+	if n < 0 {
+		return -1
+	}
+	return 1
+}
+
+func SignInt(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n < 0 {
+		return -1
+	}
+	return 1
 }
