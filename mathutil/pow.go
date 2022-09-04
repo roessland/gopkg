257871b1e84704b2a299187c829947efbb2fec6b
Andreas Røssland <andreas.roessland@gmail.com>
2022-09-04 12:22:31 +0200

    fix bug in math.Pow where an overflow was falsely detected
    The overflow check was done after an additional step, so even if the
    result was in bounds the overflow check could fail, causing a panic.

--- mathutil/pow.go
+++ mathutil/pow.go
@@ -1,15 +1,15 @@
 package mathutil
 
 // Calculates a**b for positive a and b
-func Pow(a, b int64) int64 {
-	p := int64(1)
+func Pow[T int | int64](a T, b T) T {
+	p := T(1)
 	for b > 0 {
 		if b&1 != 0 {
 			p = p * a
 		}
 		b >>= 1
 		a = a * a
-		if a < 0 {
+		if b > 0 && a < 0 {
 			panic("Integer overflow!")
 		}
 	}
--- mathutil/pow_test.go
+++ mathutil/pow_test.go
@@ -1,8 +1,20 @@
 package mathutil
 
-import "testing"
+import (
+	"testing"
+)
 import "github.com/stretchr/testify/assert"
 
 func TestPowMod(t *testing.T) {
 	assert.Equal(t, int64(3403), PowMod(353, 434, 7654), "")
+}
+
+func TestPowModStrangeCase(t *testing.T) {
+	assert.Equal(t, int64(3037001881), Pow[int64](55109, 2), "")
+}
+
+func TestPowModStrangeCases(t *testing.T) {
+	for i := int64(1); i < 5520550; i += 100 {
+		assert.Equal(t, i*i, Pow[int64](i, 2), "asdf")
+	}
 }
